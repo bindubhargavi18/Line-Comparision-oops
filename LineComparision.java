752cea53d8bc63043b67579cb9225516f2e693c3
bindubhargavi18 <bindubhargavi568@gmail.com>
2021-08-06 20:15:13 +0530

    Added:Checked equality of two lines

--- LineComparision.java
+++ LineComparision.java
@@ -3,8 +3,10 @@ class LineComparision
 {
 	public static void main(String[] args)
 	{
+		LineLength line=new LineLength();
 		int x1,x2,y1,y2;	
 		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter line1 co-ordinates:");
 		System.out.println("Enter x1:");
 		x1=sc.nextInt();
 		System.out.println("Enter x2:");
@@ -13,7 +15,18 @@ class LineComparision
 		y1=sc.nextInt();
 		System.out.println("Enter y2:");
 		y2=sc.nextInt();
-		LineLength line=new LineLength(x1,x2,y1,y2);
-		System.out.printf("length of line:%.2f ",line.length());
+		System.out.printf("length of line1:%.2f \n",line.lengthOfLine1(x1,x2,y1,y2));
+
+		System.out.println("Enter line2 co-ordinates:");
+		System.out.println("Enter x1:");
+		x1=sc.nextInt();
+		System.out.println("Enter x2:");
+		x2=sc.nextInt();
+		System.out.println("Enter y1:");
+		y1=sc.nextInt();
+		System.out.println("Enter y2:");
+		y2=sc.nextInt();
+		System.out.printf("length of line2:%.2f \n",line.lengthOfLine2(x1,x2,y1,y2));
+		line.equals();
 	}
 }
--- LineLength.java
+++ LineLength.java
@@ -2,17 +2,22 @@ import java.lang.Math;
 class LineLength
 {
 	 int x1,x2,y1,y2;
-	 double len;
-	 LineLength(int x1,int x2,int y1,int y2)
-	 {
-	 	this.x1=x1;
-	 	this.x2=x2;
-	 	this.y1=y1;
-	 	this.y2=y2;
-	 }
-	 double length()
-	 {
-	 	len=Math.sqrt(((Math.pow(x2-x1,2))+(Math.pow(y2-y1,2))));
-	 	return len;
-	 }
+	 double len1,len2;
+	public double lengthOfLine1(int x1,int x2,int y1,int y2)
+	{
+	 	len1=Math.sqrt(((Math.pow(x2-x1,2))+(Math.pow(y2-y1,2))));
+	 	return len1;
+	}
+	public double lengthOfLine2(int x1,int x2,int y1,int y2)
+	{
+		len2=Math.sqrt(((Math.pow(x2-x1,2))+(Math.pow(y2-y1,2))));
+	 	return len2;	 	
+	}
+	public void equals()
+	{
+		if(len1==len2)
+			System.out.println("Lines are equal");
+		else
+			System.out.println("Lines Not equal");
+	}
 }
